24c6528f11b0ede19af19b0b6107ca28f846e74a
Mul1x <159061027+Mul1x@users.noreply.github.com>
2026-04-01 12:54:52 +0300

    Update BOT_TOKEN in config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8455279912:AAFoWu_2-qxq-BoJUjzwXV1tcRcBnBptkhs")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8799843968:AAHmwgNjBYQJJZAI4MVlpohE4wHat3X_6zQ")
 BOT_USERNAME = os.getenv("BOT_USERNAME", "GiftsOkBot")
 SUPER_ADMIN_IDS = [int(x) for x in os.getenv("SUPER_ADMIN_IDS", "8717189451,8072028362").split(",")]
