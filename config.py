8c220ce9cafc486879e7ce97e180bf2f0481ccbf
Mul1x <159061027+Mul1x@users.noreply.github.com>
2026-04-01 19:59:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8799843968:AAHmwgNjBYQJJZAI4MVlpohE4wHat3X_6zQ")
-BOT_USERNAME = os.getenv("BOT_USERNAME", "GiftsOkBot")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "PlrkNFTBot")
 SUPER_ADMIN_IDS = [int(x) for x in os.getenv("SUPER_ADMIN_IDS", "8717189451,8072028362").split(",")]
