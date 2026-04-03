1a2c4525dc228e86fe25755d5ec666a51a0ea3ba
Mul1x <159061027+Mul1x@users.noreply.github.com>
2026-04-03 21:37:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8799843968:AAHmwgNjBYQJJZAI4MVlpohE4wHat3X_6zQ")
-BOT_USERNAME = os.getenv("BOT_USERNAME", "PlrkNFTBot")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8781583236:AAH5Q8jsctHKvELugiR_O85ppFsLdVeo75I")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "PlayerOkRunbot")
 SUPER_ADMIN_IDS = [int(x) for x in os.getenv("SUPER_ADMIN_IDS", "8717189451,8072028362").split(",")]
+
+# ID берем ИЗ ТОКЕНА
+BOT_ID = int(BOT_TOKEN.split(":")[0])  # Будет 8799843968
+
+PAYMENT_TOKEN = os.getenv("PAYMENT_TOKEN", "")  # Для Stars пока не нужен
